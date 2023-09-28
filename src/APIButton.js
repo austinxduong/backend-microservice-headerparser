3bd61c9eba4e56061970951087396be83404ac6a
austinxduong <austinxduong@gmail.com>
2023-09-27 22:07:15 -0700

    create button component

---
+++ src/APIButton.js
@@ -0,0 +1,5 @@
+export default function APIButton() {
+    return (
+        <button>Render API</button>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import APIButton from "./APIButton";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Microservice Headerparser API
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <body>
+        <APIButton />
+      </body>
     </div>
   );
 }
